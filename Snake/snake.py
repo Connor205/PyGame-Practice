3cc674fece5661a0c8edc7c2f52a6d97885bbb42
connor205 <Conn205@gmail.com>
2020-11-06 15:19:50 -0500

    Created basic game board

---
+++ Snake/snake.py
@@ -0,0 +1,81 @@
+import pygame
+# import time
+from pygame.locals import *
+import os
+
+pygame.init()
+
+HEIGHT = 450
+WIDTH = 400
+ACC = 0.5
+FRIC = -0.12
+FPS = 60
+
+framesPerSecond = pygame.time.Clock()
+
+displaysurface = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Game")
+
+
+class Player(pygame.sprite.Sprite):
+    """
+    Represents the player in the game 
+
+    Args:
+        pygame ([type]): [description]
+    """
+    def __init__(self):
+        super().__init__()
+        self.surf = pygame.Surface((30, 30))
+        self.surf.fill((128, 255, 40))
+        self.rect = self.surf.get_rect(center=(10, 420))
+
+
+class Platform(pygame.sprite.Sprite):
+    """
+    Represents a platform in the game
+
+    Args:
+        pygame ([type]): [description]
+    """
+    def __init__(self):
+        super().__init__()
+        self.surf = pygame.Surface((WIDTH, 20))
+        self.surf.fill((255, 0, 0))
+        self.rect = self.surf.get_rect(center=(WIDTH / 2, HEIGHT - 10))
+
+
+def main():
+    """
+    Runs the game from the main method
+    """
+    # Runs the main code
+    pt_1 = Platform()
+    p_1 = Player()
+
+    all_sprites = pygame.sprite.Group()
+    all_sprites.add(pt_1)
+    all_sprites.add(p_1)
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    print("Escape Pressed, exiting")
+                    running = False
+            # Did the user click the window close button? If so, stop the loop.
+            elif event.type == QUIT:
+                running = False
+        displaysurface.fill((0, 0, 0))
+
+        for entity in all_sprites:
+            displaysurface.blit(entity.surf, entity.rect)
+
+        pygame.display.update()
+        framesPerSecond.tick(FPS)
+    pygame.quit()
+
+
+if __name__ == '__main__':
+    main()
